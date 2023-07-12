a5ce6bc8a02c9bc0db02d14dbcdd151738b37d06
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-07-12 04:06:55 +0300

    all

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,7 +1,22 @@
 /*jshint esnext: true */
 /* navigation */
-$("#toggler, #overlay, #close").click(()=>{
+$("#toggler, #overlay, #close, .link").click(()=>{
   $("#menu").toggleClass("translate-x-full")
   $("body").toggleClass("overflow-hidden")
   $("#overlay").toggleClass("hidden")
 })
+
+
+/* back to top */
+let toTopButton = $("#to-top-button");
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.removeClass("hidden");
+    } else {
+        toTopButton.addClass("hidden");
+    }
+}
+
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
